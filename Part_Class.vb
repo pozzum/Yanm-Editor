1bc32a0ad005346eff7d8ebdbe88563b3274c872
pozzum <pozzumspit@yahoo.com>
2017-06-23 16:34:05 -0400

    Delete Part_Class.vb

--- Part_Class.vb
+++
@@ -1,7 +0,0 @@
-﻿Public Class Part_Class
-    Public name As String
-    Public length As Int32
-    Public Start_offset As Int32
-    Public Header_PArt As Byte() = {}
-    Public Animation_File As Byte() = {}
-End Class
